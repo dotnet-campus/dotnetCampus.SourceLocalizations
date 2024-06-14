b679432c4ff66e7d7ac522221353da7f871fc245
walterlv <xblero@gmail.com>
2024-06-14 19:13:39 +0800

    Use cultureinfo to implement ietflanguagetag

--- src/dotnetCampus.Localizations/Ietf/IetfLanguageTag.cs
+++ src/dotnetCampus.Localizations/Ietf/IetfLanguageTag.cs
@@ -10,9 +10,9 @@ namespace dotnetCampus.Localizations.Ietf;
 /// <para/>
 /// 数据由 INNA 提供，参见：https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
 /// </remarks>
-public readonly partial record struct IetfLanguageTag
+public readonly record struct IetfLanguageTag
 {
-    private readonly string _ietfLanguageTag;
+    private readonly CultureInfo _cultureInfo;
 
     /// <summary>
     /// 初始化 <see cref="IetfLanguageTag"/> 类的新实例。
@@ -20,21 +20,43 @@ public readonly partial record struct IetfLanguageTag
     /// <param name="ietfLanguageTag">符合 IETF BCP 47 标准的语言标签。</param>
     public IetfLanguageTag(string ietfLanguageTag)
     {
-        _ietfLanguageTag = ietfLanguageTag;
+        _cultureInfo = CultureInfo.GetCultureInfo(ietfLanguageTag);
+    }
+
+    /// <summary>
+    /// 初始化 <see cref="IetfLanguageTag"/> 类的新实例。
+    /// </summary>
+    /// <param name="cultureInfo">区域性信息。</param>
+    public IetfLanguageTag(CultureInfo cultureInfo)
+    {
+        _cultureInfo = cultureInfo.IsReadOnly
+            ? CultureInfo.GetCultureInfo(cultureInfo.Name)
+            : cultureInfo;
     }
 
     /// <summary>
     /// 获取当前 IETF 语言标签的字符串表示形式。
     /// </summary>
+    /// <remarks>
+    /// 虽然微软官方文档说明 <see cref="CultureInfo.IetfLanguageTag"/> 已被弃用，应改成 <see cref="CultureInfo.Name"/>，
+    /// 但实际上有且只有两个区域这两个值不同：zh-CHS 和 zh-CHT。<br/>
+    /// zh-CHS 的 Name 为 zh-CHS（被弃用）而 IetfLanguageTag 为 zh-Hans（符合标准）；
+    /// zh-CHT 的 Name 为 zh-CHT（被弃用）而 IetfLanguageTag 为 zh-Hant（符合标准）。
+    /// </remarks>
+    public string Value => _cultureInfo.IetfLanguageTag;
+
+    /// <summary>
+    /// 获取当前 IETF 语言标签的字符串表示形式。
+    /// </summary>
     /// <returns>当前 IETF 语言标签的字符串表示形式。</returns>
-    public override string ToString() => _ietfLanguageTag;
+    public override string ToString() => Value;
 
     /// <summary>
     /// 获取当前 IETF 语言标签的字符串表示形式。
     /// </summary>
     /// <param name="ietfLanguageTag">IETF 语言标签。</param>
     /// <returns></returns>
-    public static implicit operator string(IetfLanguageTag ietfLanguageTag) => ietfLanguageTag._ietfLanguageTag;
+    public static implicit operator string(IetfLanguageTag ietfLanguageTag) => ietfLanguageTag.Value;
 
     /// <summary>
     /// 从字符串创建 <see cref="IetfLanguageTag"/> 实例。
@@ -46,52 +68,5 @@ public readonly partial record struct IetfLanguageTag
     /// </param>
     /// <returns>IETF 语言标签。</returns>
     /// <exception cref="ArgumentException">字符串不符合 IETF BCP 47 标准。</exception>
-    public static implicit operator IetfLanguageTag(string ietfLanguageTag) => FromString(ietfLanguageTag, true);
-
-    /// <summary>
-    /// 从字符串创建 <see cref="IetfLanguageTag"/> 实例。
-    /// </summary>
-    /// <param name="ietfLanguageTag">符合 IETF BCP 47 标准的语言标签。</param>
-    /// <param name="autoFixWindowsLegacyCultureName">
-    /// 在 Windows 上，<see cref="CultureInfo.CurrentUICulture"/> 获取区域名称使用的是过时的 GetUserPreferredUILanguages 函数，
-    /// 此函数返回的区域名称是 Windows 自己定义的，不符合 IETF BCP 47 标准，例如 zh-CN、zh-HK、zh-TW 等。
-    /// 此函数参见：https://learn.microsoft.com/en-us/windows/win32/api/winnls/nf-winnls-getuserpreferreduilanguages
-    /// <para/>
-    /// 所以当设置此参数为 <see langword="true"/> 时，将会自动修正 Windows 的区域名称，例如将 zh-CN 修正为 zh-hans-cn、zh-HK 修正为 zh-hant-hk 等。
-    /// </param>
-    /// <returns>IETF 语言标签。</returns>
-    /// <exception cref="ArgumentException">字符串不符合 IETF BCP 47 标准。</exception>
-    public static IetfLanguageTag FromString(string ietfLanguageTag, bool autoFixWindowsLegacyCultureName = true)
-    {
-        if (IetfLanguageTags.Set.Contains(ietfLanguageTag))
-        {
-            return new IetfLanguageTag(ietfLanguageTag);
-        }
-
-        if (autoFixWindowsLegacyCultureName)
-        {
-            var fixedIetfLanguageTag = FixWindowsLegacyCultureName(ietfLanguageTag);
-            if (IetfLanguageTags.Set.Contains(fixedIetfLanguageTag))
-            {
-                return new IetfLanguageTag(fixedIetfLanguageTag);
-            }
-        }
-
-        throw new ArgumentException($"The language tag '{ietfLanguageTag}' is not a valid IETF BCP 47 language tag.", nameof(ietfLanguageTag));
-    }
-
-    /// <summary>
-    /// 修正 Windows 的区域名称。
-    /// </summary>
-    /// <param name="ietfLanguageTag"></param>
-    /// <returns></returns>
-    private static string FixWindowsLegacyCultureName(string ietfLanguageTag) => ietfLanguageTag switch
-    {
-        "zh-CN" => "zh-hans-cn",
-        "zh-HK" => "zh-hant-hk",
-        "zh-TW" => "zh-hant-tw",
-        "zh-SG" => "zh-hans-sg",
-        "zh-MO" => "zh-hant-mo",
-        _ => ietfLanguageTag,
-    };
+    public static implicit operator IetfLanguageTag(string ietfLanguageTag) => new(ietfLanguageTag);
 }
--- src/dotnetCampus.Localizations/Ietf/IetfLanguageTags.cs
+++ src/dotnetCampus.Localizations/Ietf/IetfLanguageTags.cs
@@ -3,4 +3,4 @@ namespace dotnetCampus.Localizations.Ietf;
 /// <summary>
 /// 包含所有 IETF 语言标签的字符串常量。
 /// </summary>
-public static partial class IetfLanguageTags;
+internal static partial class IetfLanguageTags;
