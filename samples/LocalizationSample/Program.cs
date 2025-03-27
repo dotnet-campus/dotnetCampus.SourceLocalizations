f8da374215c5a7a734a98d4568e9f35fdbf3460d
walterlv <xblero@gmail.com>
2025-03-27 12:56:04 +0800

    测试拿所有语言

--- samples/LocalizationSample/Program.cs
+++ samples/LocalizationSample/Program.cs
@@ -6,6 +6,9 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        var tags = Lang.SupportedLanguageTags;
+        Console.WriteLine(string.Join(", ", tags));
+
         var a = Lang.Current.A.A2.ToString(1);
         Console.WriteLine(a);
     }
